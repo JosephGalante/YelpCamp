5b2d3f99196079bd6b81f10682cfa748f588d160
Joe Galante <JEG150@pitt.edu>
2021-10-26 00:28:53 -0400

    Began to make campground page

--- app.js
+++ app.js
@@ -1,13 +1,30 @@
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
 const path = require('path');
+const campground = require('./models/campground');
 
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+    console.log('Database Connected')
+})
+
+const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
     res.render('home')
- })
+})
+app.get('/makecampground', async (req, res) => {
+    const camp = new campground({ title: 'My backyard', description: 'Cheap camping' });
+    await camp.save();
+    res.send(camp);
+})
 
 
 app.listen(3000, () => {
