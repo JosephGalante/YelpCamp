3908b0e0e673df33478240d22f9c2497ea7be3f6
Joe Galante <JEG150@pitt.edu>
2021-12-21 16:50:02 -0500

    Added Update functionality of CRUD

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 const Campground = require('./models/campground');
+const methodOverride = require('method-override');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
 	useUnifiedTopology : true
@@ -18,6 +19,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'));
 
 app.get('/', (req, res) => {
 	res.render('home');
@@ -37,17 +39,21 @@ app.post('/campgrounds', async (req, res) => {
     res.redirect(`/campgrounds/${campground._id}`)
 })
 
-
 app.get('/campgrounds/:id', async (req, res) => {
 	const campground = await Campground.findById(req.params.id);
 	res.render('campgrounds/show', { campground });
 });
 
-// app.get('/makecampground', async (req, res) => {
-//     const camp = new Campground({ title: 'My backyard', description: 'Cheap camping' });
-//     await camp.save();
-//     res.send(camp);
-// })
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+	res.render('campgrounds/edit', { campground });
+})
+
+app.put('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground }, {new: true})
+    res.redirect(`/campgrounds/${campground._id}`)
+})
 
 app.listen(3000, () => {
 	console.log('Serving on port 3000');
