fec707e9e991a3db8e4492a0f81b63fed79c3af8
Joe Galante <JEG150@pitt.edu>
2021-12-21 15:31:22 -0500

    Enhanced Create for CRUD. Added back buttons.

--- app.js
+++ app.js
@@ -4,36 +4,44 @@ const path = require('path');
 const Campground = require('./models/campground');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
-    useUnifiedTopology: true
+	useUnifiedTopology : true
 });
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
-    console.log('Database Connected');
-})
+	console.log('Database Connected');
+});
 
 const app = express();
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'))
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/', (req, res) => {
-    res.render('home')
-})
+	res.render('home');
+});
 
 app.get('/campgrounds', async (req, res) => {
-    const campgrounds = await Campground.find({});
-    res.render('campgrounds/index', { campgrounds })
+	const campgrounds = await Campground.find({});
+	res.render('campgrounds/index', { campgrounds });
+});
+
+app.get('/campgrounds/new', (req, res) => {
+	res.render('campgrounds/new');
+});
+app.post('/campgrounds', async (req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`)
 })
+
 
 app.get('/campgrounds/:id', async (req, res) => {
-
-    const campground = await Campground.findById(req.params.id)
-    res.render('campgrounds/show', { campground });
-})
-
-
-
+	const campground = await Campground.findById(req.params.id);
+	res.render('campgrounds/show', { campground });
+});
 
 // app.get('/makecampground', async (req, res) => {
 //     const camp = new Campground({ title: 'My backyard', description: 'Cheap camping' });
@@ -41,7 +49,6 @@ app.get('/campgrounds/:id', async (req, res) => {
 //     res.send(camp);
 // })
 
-
 app.listen(3000, () => {
-    console.log('Serving on port 3000')
-})
+	console.log('Serving on port 3000');
+});
