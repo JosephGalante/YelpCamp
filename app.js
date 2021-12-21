ae115809e2f38b6d466c92f22e8df6e8b545d1da
Joe Galante <JEG150@pitt.edu>
2021-12-21 14:43:32 -0500

    Established Create of CRUD functionality

--- app.js
+++ app.js
@@ -20,11 +20,17 @@ app.set('views', path.join(__dirname, 'views'));
 app.get('/', (req, res) => {
     res.render('home')
 })
-app.get('/makecampground', async (req, res) => {
-    const camp = new Campground({ title: 'My backyard', description: 'Cheap camping' });
-    await camp.save();
-    res.send(camp);
+
+app.get('/campgrounds', async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', { campgrounds })
 })
+
+// app.get('/makecampground', async (req, res) => {
+//     const camp = new Campground({ title: 'My backyard', description: 'Cheap camping' });
+//     await camp.save();
+//     res.send(camp);
+// })
 
 
 app.listen(3000, () => {
