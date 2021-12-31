0ce20637ad1c7b9c51f3aeaebd150f234beeba1a
Joe Galante <JEG150@pitt.edu>
2021-12-30 19:23:55 -0500

    Ensured campground  price has 2 decimals max

--- seeds/index.js
+++ seeds/index.js
@@ -13,25 +13,26 @@ db.once('open', () => {
 	console.log('Database Connected');
 });
 
-const sample = array => array[Math.floor(Math.random() * array.length)];
-
+const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
-    await Campground.deleteMany({});
-    for (let i = 0; i < 50; i++){
-        const random1000 = Math.floor(Math.random() * 1000);
-        const price = Math.floor(Math.random() * 30) + 10.99
-        const camp = new Campground({
-            location: `${cities[random1000].city}, ${cities[random1000].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`,
-            image: 'https://source.unsplash.com/collection/483251',
-            description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem enim illum tenetur id inventore assumenda ex fugit fugiat, exercitationem quam reprehenderit! Dolorem, illo iure ad atque veniam esse nostrum adipisci?',
-            price: price
-        })
-        await camp.save();
-    }
-}
+	await Campground.deleteMany({});
+	for (let i = 0; i < 50; i++) {
+		const random1000 = Math.floor(Math.random() * 1000);
+		let price_unrounded = Math.floor(Math.random() * 30) + 10.99;
+		let price_rounded = price_unrounded.toFixed(2);
+		const camp = new Campground({
+			location    : `${cities[random1000].city}, ${cities[random1000].state}`,
+			title       : `${sample(descriptors)} ${sample(places)}`,
+			image       : 'https://source.unsplash.com/collection/483251',
+			description :
+				'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem enim illum tenetur id inventore assumenda ex fugit fugiat, exercitationem quam reprehenderit! Dolorem, illo iure ad atque veniam esse nostrum adipisci?',
+			price       : price_rounded
+		});
+		await camp.save();
+	}
+};
 
 seedDB().then(() => {
-    mongoose.connection.close();
+	mongoose.connection.close();
 });
