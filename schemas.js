0777559a1ce0c75c1b0eb8a01b4f3d959f15544a
Joe Galante <JEG150@pitt.edu>
2021-12-31 21:39:48 -0500

    Added Pro-Tip Note for Joi Default/Required fields

--- schemas.js
+++ schemas.js
@@ -12,10 +12,14 @@ module.exports.campgroundSchema = Joi.object({
 
 let d = new Date();
 let current_full_date = `${d.getMonth() + 1}/${d.getDate()}/${d.getFullYear()}`;
+
+// Pro-Tip for Joi Schema's:
+//	Do NOT have required() and default() in the same field of a schema
+//	You only need one or the other, having both will cause issues
 module.exports.reviewSchema = Joi.object({
 	review : Joi.object({
-		rating: Joi.number().required().min(1).max(5),
-		body: Joi.string().required().min(3),
-		date: Joi.date().min(current_full_date).max(current_full_date).default(current_full_date)
+		rating : Joi.number().required().min(1).max(5),
+		body   : Joi.string().required().min(3),
+		date   : Joi.date().min(current_full_date).max(current_full_date).default(current_full_date)
 	}).required()
 });
