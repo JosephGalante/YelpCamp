42a11aaa38d736b5eb0bd375db15684e38a50232
Joe Galante <JEG150@pitt.edu>
2022-01-02 13:15:54 -0500

    Added Express Routes for Campgrounds and Reviews

--- app.js
+++ app.js
@@ -2,12 +2,10 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
-const catchAsync = require('./utilities/catchAsync');
 const ExpressError = require('./utilities/ExpressError');
-const Campground = require('./models/campground');
-const { campgroundSchema, reviewSchema } = require('./schemas.js');
 const methodOverride = require('method-override');
-const Review = require('./models/review');
+const campgrounds = require('./routes/campgrounds');
+const reviews = require('./routes/reviews');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
 	useUnifiedTopology : true
@@ -29,114 +27,13 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
-//Middleware to validate the New Campground submission
-const validateCampground = (req, res, next) => {
-	const { error } = campgroundSchema.validate(req.body);
-	if (error) {
-		const message = error.details.map((element) => element.message).join(',');
-		throw new ExpressError(message, 400);
-	} else {
-		next();
-	}
-};
-
-//Middleware to validate the Campground Review submission
-const validateReview = (req, res, next) => {
-	const { error } = reviewSchema.validate(req.body);
-	if (error) {
-		const message = error.details.map((element) => element.message).join(',');
-		throw new ExpressError(message, 400);
-	} else {
-		next();
-	}
-};
+app.use('/campgrounds', campgrounds)
+app.use('/campgrounds/:id/reviews', reviews)
 
 // Navigates to YelpCamp Home (Not home page that we use)
 app.get('/', (req, res) => {
 	res.render('home');
 });
-
-app.get(
-	'/campgrounds',
-	catchAsync(async (req, res) => {
-		const campgrounds = await Campground.find({});
-		res.render('campgrounds/index', { campgrounds });
-	})
-);
-
-app.get('/campgrounds/new', (req, res) => {
-	res.render('campgrounds/new');
-});
-
-app.post(
-	'/campgrounds',
-	validateCampground,
-	catchAsync(async (req, res, next) => {
-		const campground = new Campground(req.body.campground);
-		await campground.save();
-		res.redirect(`/campgrounds/${campground._id}`);
-	})
-);
-
-app.get(
-	'/campgrounds/:id',
-	catchAsync(async (req, res) => {
-		const campground = await Campground.findById(req.params.id).populate('reviews');
-		res.render('campgrounds/show', { campground });
-	})
-);
-
-app.get(
-	'/campgrounds/:id/edit',
-	catchAsync(async (req, res) => {
-		const campground = await Campground.findById(req.params.id);
-		res.render('campgrounds/edit', { campground });
-	})
-);
-
-app.put(
-	'/campgrounds/:id',
-	validateCampground,
-	catchAsync(async (req, res) => {
-		const { id } = req.params;
-		const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground }, { new: true });
-		res.redirect(`/campgrounds/${campground._id}`);
-	})
-);
-
-app.delete(
-	'/campgrounds/:id',
-	catchAsync(async (req, res) => {
-		const { id } = req.params;
-		await Campground.findByIdAndDelete(id);
-		res.redirect('/campgrounds');
-	})
-);
-
-app.post(
-	'/campgrounds/:id/reviews',
-	validateReview,
-	catchAsync(async (req, res) => {
-		const campground = await Campground.findById(req.params.id);
-		const review = new Review(req.body.review);
-		campground.reviews.push(review);
-		await review.save();
-		await campground.save();
-		res.redirect(`/campgrounds/${campground._id}`);
-	})
-);
-
-app.delete('/campgrounds/:campId/reviews/:reviewId', catchAsync(async (req, res) => {
-
-	// Note (Not sure if this is set in stone or I'm doing something wrong): 
-	// When destructuring req.params, the variable names MUST BE the same as
-	// The keys in req.params for some reason
-	const { campId, reviewId } = req.params;
-	await Campground.findByIdAndUpdate(campId, { $pull: { reviews: reviewId } })
-	await Review.findByIdAndDelete(reviewId);
-	
-	res.redirect(`/campgrounds/${campId}`);
-}) )
 
 app.all('*', (req, res, next) => {
 	// res.send('404 Error')
