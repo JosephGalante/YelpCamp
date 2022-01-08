0128aa2af88c87ef27ac71c1957fb155c39e27ea
Joe Galante <JEG150@pitt.edu>
2022-01-07 20:37:14 -0500

    Removed geocode API calls and used cities.js data

--- seeds/index.js
+++ seeds/index.js
@@ -3,10 +3,6 @@ const cities = require('./cities');
 const { places, descriptors } = require('./seedHelpers');
 const Campground = require('../models/campground');
 
-const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
-const mapBoxToken = 'pk.eyJ1IjoiamVnMTUwIiwiYSI6ImNreTR2OWw0dzAyYXYyenJ6aXQza3BrNXkifQ.9K4bZAFWce-jacKnvLomXw'
-const geocoder = mbxGeocoding({ accessToken: mapBoxToken });
-
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
 	useUnifiedTopology : true
 });
@@ -26,27 +22,20 @@ const seedDB = async () => {
 		const price_unrounded = Math.floor(Math.random() * 30) + 10.99;
 		const price_rounded = price_unrounded.toFixed(2);
 
-		const geoData = await geocoder.forwardGeocode({
-			query: `${cities[random1000].city}, ${cities[random1000].state}`,
-			limit: 1
-		})
-		.send();
-		const coords = geoData.body.features[0].geometry.coordinates;
-
 		const camp = new Campground({
 			author      : '61d611d58e6a547c925c777d',
 			location    : `${cities[random1000].city}, ${cities[random1000].state}`,
 			title       : `${sample(descriptors)} ${sample(places)}`,
 			description : 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quidem enim illum tenetur id inventore assumenda ex fugit fugiat, exercitationem quam reprehenderit! Dolorem, illo iure ad atque veniam esse nostrum adipisci?',
 			price: price_rounded,
+			geometry: {
+				type: "Point",
+				coordinates: [cities[random1000].longitude, cities[random1000].latitude]
+			},
 			images: [{
       			url: 'https://res.cloudinary.com/dlbgh2yop/image/upload/v1641597954/YelpCamp/yosemite_ulnhap.jpg',
       			filename: 'YelpCamp/duc2s3rlslzmvewaghzj'
-			}],
-			geometry: {
-				type: "Point",
-				coordinates: coords
-			}
+			}]
 		});
 		await camp.save();
 	}
