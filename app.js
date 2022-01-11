9ae9536b071f781c2698b22a87b8c818a89842ba
Joe Galante <JEG150@pitt.edu>
2022-01-10 23:41:56 -0500

    Added helmet security

--- app.js
+++ app.js
@@ -14,12 +14,14 @@ const LocalStrategy = require('passport-local');
 const User = require('./models/user');
 const passport = require('passport');
 const mongoSanitize = require('express-mongo-sanitize');
-
+const helmet = require('helmet');
+const MongoDBStore = require('connect-mongo');
 
 const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 const userRoutes = require('./routes/users');
-
+// const dbUrl = process.env.DB_URL;
+//'mongodb://localhost:27017/yelp-camp'
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
 	useUnifiedTopology : true
 });
@@ -44,11 +46,13 @@ app.use(mongoSanitize({
 }));
 
 const sessionConfig = {
+	name              : 'session',
 	secret            : 'Secret Agent Randy Beans',
 	resave            : false,
 	saveUninitialized : true,
 	cookie            : {
-		httpOnly : true,
+ 		httpOnly: true,
+		// secure: true,
 		//That product is 1 week (7 days)
 		expires  : Date.now() + 1000 * 60 * 60 * 24 * 7,
 		maxAge   : 1000 * 60 * 60 * 24 * 7
@@ -56,6 +60,66 @@ const sessionConfig = {
 };
 app.use(session(sessionConfig));
 app.use(flash());
+const scriptSrcUrls = [
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://api.mapbox.com/",
+	"https://kit.fontawesome.com/",
+	"https://ka-f.fontawesome.com",
+    "https://kit.fontawesome.com",
+    "https://cdnjs.cloudflare.com/",
+    "https://cdn.jsdelivr.net/",
+    "https://res.cloudinary.com/dlbgh2yop/"
+];
+const styleSrcUrls = [
+    "https://kit-free.fontawesome.com/",
+    "https://stackpath.bootstrapcdn.com/",
+    "https://api.mapbox.com/",
+    "https://api.tiles.mapbox.com/",
+    "https://fonts.googleapis.com/",
+    "https://use.fontawesome.com/",
+    "https://cdn.jsdelivr.net/",
+    "https://res.cloudinary.com/dlbgh2yop/"
+];
+const connectSrcUrls = [
+    "https://*.tiles.mapbox.com",
+    "https://api.mapbox.com",
+    "https://events.mapbox.com",
+	"https://res.cloudinary.com/dlbgh2yop/",
+	"https://use.fontawesome.com/",
+	"https://ka-f.fontawesome.com"
+];
+const fontSrcUrls = [
+	"https://res.cloudinary.com/dlbgh2yop/",
+	"https://ka-f.fontawesome.com",
+	"https://kit.fontawesome.com",
+	"https://kit.fontawesome.com/"
+];
+ 
+app.use(
+    helmet.contentSecurityPolicy({
+        directives : {
+            "default-src" : [],
+            "connect-src" : [ "'self'", ...connectSrcUrls ],
+            scriptSrc  : [ "'unsafe-inline'", "'self'", ...scriptSrcUrls ],
+            styleSrc   : [ "'self'", "'unsafe-inline'", ...styleSrcUrls ],
+            workerSrc  : [ "'self'", "blob:" ],
+            objectSrc  : [],
+            "img-src"     : [
+                "'self'",
+                "blob:",
+                "data:",
+                "https://res.cloudinary.com/dlbgh2yop/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT!
+				"https://images.unsplash.com/",
+				
+            ],
+            fontSrc    : [ "'self'", ...fontSrcUrls ],
+            mediaSrc   : [ "https://res.cloudinary.com/dlbgh2yop/" ],
+            childSrc   : [ "blob:" ]
+        }
+    })
+);
+
 app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
